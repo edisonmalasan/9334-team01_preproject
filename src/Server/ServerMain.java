ec620067b0f878d679a48191a3b6504ac081947e
Eruki <malasanedison1@gmail.com>
2025-02-01 23:53:07 +0800

    minor changes

--- src/Server/ServerMain.java
+++ src/Server/ServerMain.java
@@ -6,13 +6,15 @@ import Server.model.XMLStorageModel;
 import java.io.IOException;
 
 public class ServerMain {
+
+    private static final int PORT_NUMBER = 5000;
+
     public static void main(String[] args) throws IOException {
         QuestionBankModel questionBank = new QuestionBankModel();
         XMLStorageModel.loadQuestionFromXML("data/question.xml", questionBank);
 
         System.out.println("QuestionBank loaded: " + questionBank.getAllQuestions().size());
 
-        int PORT_NUMBER = 5000;
         ServerHandler server = new ServerHandler(PORT_NUMBER, questionBank);
         server.start();
     }

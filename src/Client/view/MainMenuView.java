0362db210eabd1a2dd2e424616e57075bfa0a15a
Eruki <malasanedison1@gmail.com>
2025-02-04 22:29:00 +0800

    combo controller and combo view

--- src/Client/view/MainMenuView.java
+++ src/Client/view/MainMenuView.java
@@ -1,4 +1,10 @@
 package Client.view;
 
+import javax.swing.*;
+
 public class MainMenuView {
+    public JButton startButton;
+    public JButton leaderboardButton;
+    public JButton exitButton;
+
 }

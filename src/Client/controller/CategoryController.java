672b28ad1726b88d737528b58ad4766275c45abd
Eruki <malasanedison1@gmail.com>
2025-02-05 21:59:57 +0800

    category controller

--- src/Client/controller/CategoryController.java
+++ src/Client/controller/CategoryController.java
@@ -1,4 +1,22 @@
 package Client.controller;
 
+import Client.connection.ClientConnection;
+import Client.view.GameView;
+
 public class CategoryController {
+    private ClientConnection clientConnection;
+
+    public CategoryController(ClientConnection clientConnection) {
+        this.clientConnection = clientConnection;
+    }
+
+    public void handleCategorySelection(String category) {
+        String question = clientConnection.sendRequest("GET_QUESTION: " + category);
+        if (question != null) {
+            new GameView(clientConnection, question);
+        } else {
+            System.out.println("No question found");
+            // (optional) show a toast message here from javafx instead of sout if there is no question in the data
+        }
+    }
 }

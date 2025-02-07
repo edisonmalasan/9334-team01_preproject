e7819512e391da8c99ceab9c171b305ceb6e6c6c
Eruki <malasanedison1@gmail.com>
2025-02-07 21:51:42 +0800

    git alias test

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -4,4 +4,7 @@ public class Protocol {
     public static final String GET_QUESTION = "GET_QUESTION";
     public static final String GET_LEADERBOARD = "GET_LEADERBOARD";
     public static final String ADD_SCORE = "ADD_SCORE";
+
+
+    // BADING SI JIM
 }

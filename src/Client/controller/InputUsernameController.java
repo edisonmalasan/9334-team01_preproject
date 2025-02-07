821d34bf3ee39d2ae092b4d40e3c4b4470cd670a
DANNIEVIC LUACAN <2240879@slu.edu.ph>
2025-02-07 00:51:23 +0000

    Edit InputUsernameController.java

--- src/Client/controller/InputUsernameController.java
+++ src/Client/controller/InputUsernameController.java
@@ -3,8 +3,8 @@ package Client.controller;
 public class InputUsernameController() {
     private String username;
 
-    public void setUsername() {
-    
+    public void setUsername(String username) {
+        this.username = username;
     }
     public String getUsername() {
         return "";

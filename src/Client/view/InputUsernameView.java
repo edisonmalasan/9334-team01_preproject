c576e8c4d9fdb7ada7732fcb346c352aa7a54f8d
Personal <2240879@slu.edu.ph>
2025-02-09 15:45:22 +0800

    Added sample GUI for InputUsernameView and added constructor for LeaderboardView

--- src/Client/view/InputUsernameView.java
+++ src/Client/view/InputUsernameView.java
@@ -1,4 +1,29 @@
 package Client.view;
 
-public class InputUsernameView {
+//TODO: For testing only, to be improved
+
+import javax.swing.*;
+import java.awt.*;
+
+public class InputUsernameView extends JFrame {
+
+    public JLabel enterUsernameLabel;
+    public JTextField usernameField;
+    public JButton enterButton;
+    public InputUsernameView() {
+        setTitle("Username Input");
+        setSize(1024, 768);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setVisible(true);
+        setLayout(new GridLayout(3,1));
+
+        enterUsernameLabel = new JLabel("Enter your username:");
+        usernameField = new JTextField();
+        enterButton = new JButton("Enter");
+
+        add(enterUsernameLabel);
+        add(usernameField);
+        add(enterButton);
+    }
 }
--- src/Client/view/LeaderboardView.java
+++ src/Client/view/LeaderboardView.java
@@ -8,4 +8,7 @@ public class LeaderboardView {
     public LeaderboardView(ClientConnection clientConnection) {
         this.clientConnection = clientConnection;
     }
+
+    public LeaderboardView (){
+    }
 }

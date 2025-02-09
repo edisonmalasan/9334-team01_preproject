5d20a6893ad3952e525043f1d984c3896a053ee3
Personal <2240879@slu.edu.ph>
2025-02-10 00:46:11 +0800

    Added protocol for GET_CHOICES (getting question choices)

--- src/common/Protocol.java
+++ src/common/Protocol.java
@@ -2,6 +2,7 @@ package common;
 
 public class Protocol {
     public static final String GET_QUESTION = "GET_QUESTION";
+    public static final String GET_CHOICES = "GET_CHOICES";
     public static final String GET_LEADERBOARD = "GET_LEADERBOARD";
     public static final String ADD_SCORE = "ADD_SCORE";
 

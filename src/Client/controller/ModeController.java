724dfcfaea5e65a0c831ed7d51bd370f46b52d6e
Eruki <malasanedison1@gmail.com>
2025-02-13 01:28:45 +0800

    removed clientmain

--- src/Client/controller/ModeController.java
+++ src/Client/controller/ModeController.java
@@ -8,8 +8,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class ModeController {
     @FXML
     public ImageView gamemodeLabel;

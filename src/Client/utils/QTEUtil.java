fb22a2dd497d603b4df8d7a8052f62d49763e9b6
Eruki <malasanedison1@gmail.com>
2025-02-16 20:33:04 +0800

    timer integration and QTEEvent that randomly pops up on screen

--- src/Client/utils/QTEUtil.java
+++ src/Client/utils/QTEUtil.java
@@ -1,6 +1,5 @@
 package Client.utils;
 
-import Client.utils.TimerUtil;
 import javafx.application.Platform;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
@@ -51,19 +50,19 @@ public class QTEUtil {
                     "-fx-background-color: transparent; " +
                     "-fx-pref-width: 200px; -fx-pref-height: 80px;");
 
-            // 🎯 Get QTEPane dimensions to calculate random position
-            double maxX = QTEPane.getPrefWidth() - 200;  // Ensure button stays within bounds
+            // random pop up
+            double maxX = QTEPane.getPrefWidth() - 200;
             double maxY = QTEPane.getPrefHeight() - 80;
 
-            // 🔥 Random position
             double randomX = random.nextDouble() * maxX;
             double randomY = random.nextDouble() * maxY;
 
             defuseButton.setLayoutX(randomX);
             defuseButton.setLayoutY(randomY);
             QTEPane.getChildren().add(defuseButton);
+            defuseButton.toFront(); // move button forward if the position is in another pane
 
-            // Timer for QTE failure
+            //timer qte fail
             TimerTask failTask = new TimerTask() {
                 @Override
                 public void run() {
@@ -78,7 +77,7 @@ public class QTEUtil {
             };
             timer.schedule(failTask, 3000); // 3 seconds to react
 
-            // If clicked in time, change image & remove
+            // change image and remove
             defuseButton.setOnAction(e -> {
                 defuseButton.setStyle("-fx-background-image: url('/images/defuse_button_clicked.png'); "  +
                         "-fx-background-size: contain; " +
@@ -86,7 +85,7 @@ public class QTEUtil {
                         "-fx-background-color: transparent; " +
                         "-fx-pref-width: 200px; -fx-pref-height: 80px;");
 
-                // Delay before removal for smooth effect
+                // delay before removal
                 new Timer().schedule(new TimerTask() {
                     @Override
                     public void run() {

c511c3d831dd63d4c7c82a2f3b678e5869d431ad
PRINCESS KYLA ROSE FERRER <2240711@slu.edu.ph>
2025-02-17 10:33:51 +0000

    mainMenu Contoller

--- src/Client/controller/MainMenuController.java
+++ src/Client/controller/MainMenuController.java
@@ -1,5 +1,39 @@
 package Client.controller;
 
+import Client.connection.AnsiFormatter;
+import Client.view.CategoryView;
+import Client.view.MainMenuView;
+import Client.view.ModeView;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class MainMenuController {
-    // todo by kyla
+    private static final Logger logger = Logger.getLogger(MainMenuController.class.getName());
+
+    static {
+        AnsiFormatter.enableColorLogging(logger);
+    }
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button leaderboardButton;
+
+    @FXML
+    private Button quitButton;
+
+    private MainMenuView mainMenuView;
+
+    public void setMainMenuView(MainMenuView mainMenuView) {
+        this.mainMenuView = mainMenuView;
+    }
 }

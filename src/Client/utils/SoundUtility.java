1916d53307d827ada3403204ae9489eb258866b2
Yuen, Ka Yue Ymanuelle T. <2235608@slu.edu.ph>
2025-02-18 10:26:26 +0800

    Added audio clip

--- src/Client/utils/SoundUtility.java
+++ src/Client/utils/SoundUtility.java
@@ -1,7 +1,5 @@
 package Client.utils;
 
-import javafx.scene.media.MediaPlayer;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
@@ -18,28 +16,27 @@ public class SoundUtility {
     Clip clip;
     // current status of clip
     String status;
-    AudioInputStream audioInputStream;
-    static String filePath;
+    AudioInputStream in1, in2;
+    static String bgmPath, clickPath;
 
-    private static MediaPlayer mediaPlayer;
     public SoundUtility() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         // create AudioInputStream object
-        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+        in1 = AudioSystem.getAudioInputStream(new File(bgmPath).getAbsoluteFile());
+        in2 = AudioSystem.getAudioInputStream(new File(clickPath).getAbsoluteFile());
+
         // create clip reference
         clip = AudioSystem.getClip();
         // open audioInputStream to the clip
-        clip.open(audioInputStream);
+        clip.open(in1);
         clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
     // Play the background music
     public static void playBackgroundMusic() {
-        try
-        {
-            filePath = "resources/audio/bgm.wav";
-            SoundUtility audioPlayer =
-                    new SoundUtility();
-            audioPlayer.play();
+        try {
+            bgmPath = "resources/audio/bgm.wav";
+            SoundUtility audioPlayer1 = new SoundUtility();
+            audioPlayer1.play();
             Scanner sc = new Scanner(System.in);
 
             sc.close();
@@ -49,16 +46,20 @@ public class SoundUtility {
         }
     }
 
-    public void play() {
-        //start the clip
-        clip.start();
-        status = "play";
-    }
-    // Stop the background music
-    public static void stopBackgroundMusic() {
-        if (mediaPlayer != null) {
-            mediaPlayer.stop();
+    public static void playClickingSound() {
+        try {
+            clickPath = "resources/audio/Click.wav";
+            SoundUtility audioPlayer2 = new SoundUtility();
+            audioPlayer2.play();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
         }
+    }
+        public void play() {
+            //start the clip
+            clip.start();
+            status = "play";
     }
 }
 

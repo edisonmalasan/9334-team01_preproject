9201f6b49375b11f6d81b362db81c81c4c5f9f3f
Yuen, Ka Yue Ymanuelle T. <2235608@slu.edu.ph>
2025-02-18 09:51:51 +0800

    Fixed audio

--- src/Client/utils/SoundUtility.java
+++ src/Client/utils/SoundUtility.java
@@ -1,46 +1,59 @@
 package Client.utils;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaException;
 import javafx.scene.media.MediaPlayer;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 public class SoundUtility {
+    // to store current position
+    Long currentFrame;
+    Clip clip;
+    // current status of clip
+    String status;
+    AudioInputStream audioInputStream;
+    static String filePath;
 
     private static MediaPlayer mediaPlayer;
+    public SoundUtility() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        // create AudioInputStream object
+        audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+        // create clip reference
+        clip = AudioSystem.getClip();
+        // open audioInputStream to the clip
+        clip.open(audioInputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
 
     // Play the background music
-    public static void playBackgroundMusic(String musicFileName) {
-        try {
-            // Stop the old music if it's playing
-            if (mediaPlayer != null) {
-                mediaPlayer.stop();
-                mediaPlayer.dispose();  // Dispose to release resources
-            }
-            // Dynamically create the path based on the given file name
-            String musicPath = "resources/audio/bgm.wav" + musicFileName;
+    public static void playBackgroundMusic() {
+        try
+        {
+            filePath = "resources/audio/bgm.wav";
+            SoundUtility audioPlayer =
+                    new SoundUtility();
+            audioPlayer.play();
+            Scanner sc = new Scanner(System.in);
 
-            String bgmPath = SoundUtility.class.getResource(musicPath).toExternalForm();
-
-            if (bgmPath == null) {
-                System.err.println("Error: Could not find the background music file: " + musicFileName);
-                return;  // Exit if the file is not found
-            }
-
-            System.out.println("Playing BGM from: " + bgmPath);
-
-            Media bgm = new Media(bgmPath);
-            mediaPlayer = new MediaPlayer(bgm);
-
-            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);  // Loop indefinitely
-            mediaPlayer.play();
-
-        } catch (MediaException e) {
-            System.err.println("Error while initializing the MediaPlayer: " + e.getMessage());
-        } catch (NullPointerException e) {
-            System.err.println("Error: Media file path is incorrect or the file is missing.");
+            sc.close();
+        } catch (Exception ex) {
+            System.out.println("Error with playing sound.");
+            ex.printStackTrace();
         }
     }
 
+    public void play() {
+        //start the clip
+        clip.start();
+        status = "play";
+    }
     // Stop the background music
     public static void stopBackgroundMusic() {
         if (mediaPlayer != null) {
